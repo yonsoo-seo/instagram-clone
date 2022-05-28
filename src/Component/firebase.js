44cc8b285183fc18b8d2ad80d183e05ff144ab3b
Yonsoo Seo <99024754+yonsoo-seo@users.noreply.github.com>
2022-05-28 19:34:43 +0900

    Update firebase.js

--- src/Component/firebase.js
+++ src/Component/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/storage";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDwh1UMTwJ6k7eSxx-POj4afO6svYGRhpk",
-  authDomain: "instagram-clone-aa309.firebaseapp.com",
-  projectId: "instagram-clone-aa309",
-  torageBucket: "instagram-clone-aa309.appspot.com",
-  messagingSenderId: "472854624684",
-  appId: "1:472854624684:web:4f127031e9f18bf648e209",
+  apiKey: "<api-Key>",
+  authDomain: "<auth-Domain>",
+  projectId: "<project-Id>",
+  storageBucket: "<storage-Bucket>",
+  messagingSenderId: "<messaging-Sender-Id>",
+  appId: "<app-Id>",
 });
 
 export const auth = firebase.auth();
